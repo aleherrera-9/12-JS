0106393180f301dc7f252464f629ef7a77d57ae7
alejandra.herrera <alejandra.herrera@accenture.com>
2021-12-13 11:25:26 -0300

    new update

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Product {
       <div class="col">
       <div class="card m-3  p-1 text-center" id="product${this.productId}"style="width: 18rem;">
     
-      <a ><img src="./Multimedia/product${this.imgId}.jpg"class="card-img-top" alt="..."></a>
+      <a ><img id="img${this.imgId}" src="./Multimedia/product${this.imgId}.jpg"class="card-img-top" alt="..."></a>
      
       <div class="card-body"  >
         <h5 class="card-title"><strong>${this.name}</strong></h5>
@@ -41,54 +41,104 @@ const product10 = new Product(10, "Demon Slayer", 0, priceEmbrodery, 10);
 const product11 = new Product(11, "Given", 0, priceEmbrodery, 11);
 const product12 = new Product(12, "Amorfo", 0, priceEmbrodery, 12);
 let productList2 = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12];
-
+//animate, SlideDo0wn y css agregado
 $(() => {
   productList2.forEach(oneProduct => {
     $('#list').append(oneProduct.nuevo());
     $(`#shop${oneProduct.productId}`).on('click', () => {
-      productList.push(new Product(oneProduct.productId, oneProduct.name, 1, oneProduct.price, oneProduct.imgId));
-      let resume = JSON.parse(localStorage.getItem("Productos"));
-      if (resume) {
-        resume = resume.concat(productList);
-        localStorage.setItem("Productos", JSON.stringify(resume));
-      }
-      else {
-        localStorage.setItem("Productos", JSON.stringify(productList));
-      }
-      $(`#text${oneProduct.productId}`).append(`<br><p class="text-center  btn-outline-success text-white fs-5">${oneProduct.name} agregado al carrito</p>`)
+      $(`#img${oneProduct.imgId}`).animate({
+        width:"200px",
+        heigth:"200px"
+      })
+      All();
+      list(oneProduct);
+      finalPrice();
+      $(`#text${oneProduct.productId}`).append(`<br><p class="text-center  btn-outline-success text-white fs-5">${oneProduct.name} agregado al carrito</p><div class="text-center p-3 m-2" ><button id="boton2" class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-basket" viewBox="0 0 16 16">
+      <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
+    </svg></button></div><p>Total: ${finalPrice()}</p>`)
+      $(`#text${oneProduct.productId}`).slideDown("slow");
       setTimeout(function () {
-        location.href = "index.html";
-      }, 1000)
+        $(`#img${oneProduct.imgId}`).css({
+          width:"276px",
+          heigth:"367px"
+        })
+        $(`#text${oneProduct.productId}`).toggle("slow");
+        $(`#text${oneProduct.productId}`).empty();
+      }, 2000)
     })
   })
 
 })
-$(()=>{
-  let resume = JSON.parse(localStorage.getItem("Productos"));
-if (resume) {
-  $('#new').append(`<div class="text-center p-3 m-2" ><button id="boton2" class="btn btn-outline-primary">Ver carrito</button></div>`)
-  $('#boton2').on('click', () => {
-    $('#new').empty();
-    $('#new1').append(`<div class="text-center p-3 m-2" ><button id="boton4" class="btn btn-outline-primary">Seguir comprando</button> <button id="boton3"  class="btn btn-outline-primary">Vaciar carrito</button>`)
-    $('#boton4').on('click', () => {
-      location.href = "index.html";
-    })
-    $('#list').empty();
-    resume.forEach(item => {
-      $('#list').append(`<div class="col">
-        <div class="card m-3  p-1 text-center" id="product${item.productId}"style="width: 18rem;">
-        <a ><img src="./Multimedia/product${item.imgId}.jpg"class="card-img-top" alt="..."></a>
-        <div class="card-body"  >
-        <h5 class="card-title"><strong>${item.name}</strong></h5>
-        <p class="card-text">Precio: ${item.price} </p>
-      </div> `)
-    })
-    $('#boton3').on('click', () => {
-      localStorage.clear();
-      location.href = "index.html";
-    })
+function finalPrice(){
+  let products = JSON.parse(localStorage.getItem("Productos"));
+  let priceF=0;
+  products.forEach(item=>{
+    priceF+=item.price;
   })
- 
+  return priceF;
 }
+function list(oneProduct) {
+  let productResume=[];
+  let products = JSON.parse(localStorage.getItem("Productos"));
+  if (products) {
+    products.push(new Product(oneProduct.productId, oneProduct.name, 1, oneProduct.price, oneProduct.imgId));
+    localStorage.setItem("Productos", JSON.stringify(products));
+  } else {
+    productResume.push(new Product(oneProduct.productId, oneProduct.name, 1, oneProduct.price, oneProduct.imgId));
+    localStorage.setItem("Productos", JSON.stringify(productResume));
+  }
+}
+function All(){
+  $(()=>{
+    let resume = JSON.parse(localStorage.getItem("Productos"));
+  if (resume) {
+    $('#boton2').on('click', () => {
+      $('#new1').append(`<div class="text-center p-3 m-2" ><button id="boton4" class="btn btn-outline-primary">Seguir comprando</button> <button id="boton3"  class="btn btn-outline-primary">Vaciar carrito</button>`)
+      $('#new').append(`<p class="text-center text-warning fs-4">Total: ${finalPrice()} </p>`)
+      $('#boton4').on('click', () => {
+        location.href = "index.html";
+      })
+      $('#list').empty();
+      resume.forEach(item => {
+        $('#list').append(`<div class="col">
+          <div class="card m-3  p-1 text-center" id="product${item.productId}"style="width: 18rem;">
+          <a ><img src="./Multimedia/product${item.imgId}.jpg"class="card-img-top" alt="..."></a>
+          <div class="card-body"  >
+          <h5 class="card-title"><strong>${item.name}</strong></h5>
+          <p class="card-text">Precio: ${item.price} </p>
+        </div> `)
+      })
+      $('#boton3').on('click', () => {
+        localStorage.clear();
+        location.href = "index.html";
+      })
+    })
+   
+  }
+  })
+}
+$(()=>{
+let resume = JSON.parse(localStorage.getItem("Productos"));
+  if (resume) {
+    $('#new').on('click', () => {
+      $('#new1').append(`<div class="text-center p-3 m-2" ><button id="boton4" class="btn btn-outline-primary">Seguir comprando</button> <button id="boton3"  class="btn btn-outline-primary">Vaciar carrito</button>`)
+      $('#boton4').on('click', () => {
+        location.href = "index.html";
+      })
+      $('#list').empty();
+      resume.forEach(item => {
+        $('#list').append(`<div class="col">
+          <div class="card m-3  p-1 text-center" id="product${item.productId}"style="width: 18rem;">
+          <a ><img src="./Multimedia/product${item.imgId}.jpg"class="card-img-top" alt="..."></a>
+          <div class="card-body"  >
+          <h5 class="card-title"><strong>${item.name}</strong></h5>
+          <p class="card-text">Precio: ${item.price} </p>
+        </div> `)
+      })
+      $('#boton3').on('click', () => {
+        localStorage.clear();
+        location.href = "index.html";
+      })
+    })
+  }
 })
-
